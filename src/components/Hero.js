8b98158966a97a5edb66aac6290cafe40ec17d6b
Shiraz Ayman <sheraz.ayman@gmail.com>
2023-07-04 10:36:34 +0300

    updatedd

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Hero.css';
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-content">
         <h1 className="hero-title"> Share and Discover Inspiring Stories</h1>
-        <button className="CTA-btn">
+        <Link to="Post" className="CTA-btn">
             Get started
-          </button>
+          </Link>
       </div>
     </div>
   );
